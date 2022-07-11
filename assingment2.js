ca2c25d5614b92f7c6fc4f0263ee6771ad9bd162
CoreyDeCaire <coreydecaire19@gmail.com>
2022-07-11 19:45:19 -0400

    added more styling to darkmode

--- assingment2.js
+++ assingment2.js
@@ -1,3 +1,6 @@
+// I first attempted to make the slide out navBar close using the X. I tried the code below (as well as many different versions but couldn't get it to close.) I know there is a way to target the button and then have it hide/show using a toggle status but couldn't make it work. I left in the code but blocked it off just to show what I was up to/attempting.
+
+
 // const closeButton = document.querySelector('button')
 // console.log(closeButton)
 
@@ -6,9 +9,14 @@
 //     closeNav = toggle
 // })
 
+
+// create a Variable to select my Button
 const darkMode = document.querySelector('button')
+// Add an event listener for the 'click' of the button and run the toggle function to change between regular mode and dark mode.
 darkMode.addEventListener('click', function() {
     document.body.classList.toggle('darkModeStyle')
 })
+// Left in the console log to show the element I was trying to manipulate.
 console.log(darkMode)
-
+// To complete this I used _baseStyles.scss and put in a .darkModeStyle and added some style to the button.
+// I also added a button in my HTML
