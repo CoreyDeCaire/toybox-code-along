beeed5a0ca4e42439030322ef9eecbfa35884014
CoreyDeCaire <coreydecaire19@gmail.com>
2022-07-11 18:54:20 -0400

    added a dark mode button to homepage

---
+++ assingment2.js
@@ -0,0 +1,14 @@
+// const closeButton = document.querySelector('button')
+// console.log(closeButton)
+
+// closeButton.addEventListener('click', function(event){
+//     event.preventDefault();
+//     closeNav = toggle
+// })
+
+const darkMode = document.querySelector('button')
+darkMode.addEventListener('click', function() {
+    document.body.classList.toggle('darkModeStyle')
+})
+console.log(darkMode)
+
